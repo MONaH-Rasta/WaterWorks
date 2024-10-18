f4be7a35e72b0c70a51c37502b249a3c2116e4c2
MON@H <MONaH.Rasta@gmail.com>
2024-10-18 07:04:51 +0300

    v1.0.5

--- WaterWorks.cs
+++ WaterWorks.cs
@@ -2,9 +2,14 @@
 using System;
 using System.Linq;
 
+/*
+Restructed some of the config which requires you to reconfigure it
+Fixed stack size amounts and values applied to configuration defaults
+*/
+
 namespace Oxide.Plugins
 {
-    [Info("Water Works", "nivex", "1.0.4")]
+    [Info("Water Works", "nivex", "1.0.5")]
     [Description("Control the monopoly on your water supplies.")]
     class WaterWorks : RustPlugin
     {
@@ -76,7 +81,7 @@ namespace Oxide.Plugins
                 lc.inventory.MarkDirty();
                 lc.SendNetworkUpdateImmediate();
             }
-            else if (lc is WaterCatcher && _config.WaterCatcherInterval > 0)
+            else if (lc is WaterCatcher)
             {
                 UpdateWaterCatcher(lc as WaterCatcher, state);
             }
@@ -107,10 +112,10 @@ namespace Oxide.Plugins
                 purifier.inventory.MarkDirty();
                 purifier.SendNetworkUpdateImmediate();
             }
-            else if (lc is WaterPump && _config.WaterPump > 0)
+            else if (lc is WaterPump && _config.WaterPump.StackSize > 0)
             {
                 var pump = lc as WaterPump;
-                int num = state ? _config.WaterPump : 2000;
+                int num = state ? _config.WaterPump.StackSize : 2000;
 
                 SetSlotAmounts(lc, num);
 
@@ -126,7 +131,7 @@ namespace Oxide.Plugins
         private void ConfigureWaterDefinitions(bool state)
         {
             if (bucketWaterMod != null) bucketWaterMod.maxStackSize = state ? _config.WaterBucket : 2000;
-            if (waterJugMod != null) waterJugMod.maxStackSize = state ? _config.WaterJug : 4000;
+            if (waterJugMod != null) waterJugMod.maxStackSize = state ? _config.WaterJug : 5000;
             if (waterDef != null) waterDef.stackable = state ? _config.WaterJug * 62 : 249999;
         }
 
@@ -134,44 +139,93 @@ namespace Oxide.Plugins
         {
             if (state)
             {
-                int num = wc.prefabID == 3418194637 ? _config.StackSizeLarge : _config.StackSizeSmall;
-                SetSlotAmounts(wc, num);
-                wc.maxItemToCreate = _config.WaterCatcherAmount;
-                wc.inventory.maxStackSize = num;
-                wc.Invoke(() =>
-                {
-                    if (wc.IsDestroyed) return;
-                    wc.CancelInvoke(wc.CollectWater);
-                    wc.InvokeRepeating(wc.CollectWater, _config.WaterCatcherInterval, _config.WaterCatcherInterval);
-                }, 0.1f);
+                wc.maxStackSize = wc.inventory.maxStackSize = wc.prefabID == 3418194637 ? _config.LargeWaterCatcher.StackSize : _config.SmallWaterCatcher.StackSize;
+                wc.maxItemToCreate = wc.prefabID == 3418194637 ? _config.LargeWaterCatcher.Amount : _config.SmallWaterCatcher.Amount;
             }
             else
             {
-                int num = wc.prefabID == 3418194637 ? 50000 : 10000;
-                SetSlotAmounts(wc, num);
-                wc.maxStackSize = num;
-                wc.Invoke(() =>
-                {
-                    if (wc.IsDestroyed) return;
-                    wc.CancelInvoke(wc.CollectWater);
-                    wc.InvokeRandomized(wc.CollectWater, WaterCatcher.collectInterval, WaterCatcher.collectInterval, 6f);
-                }, 0.1f);
+                wc.maxStackSize = wc.inventory.maxStackSize = wc.prefabID == 3418194637 ? 50000 : 10000;
+                wc.maxItemToCreate = wc.prefabID == 3418194637 ? 30 : 10;
             }
+
+            SetSlotAmounts(wc, wc.maxStackSize);
+
+            wc.Invoke(() =>
+            {
+                if (wc.IsDestroyed) return;
+                wc.CancelInvoke(wc.CollectWater);
+
+                if (state)
+                {
+                    float interval = wc.prefabID == 3418194637 ? _config.LargeWaterCatcher.Interval : _config.SmallWaterCatcher.Interval;
+                    wc.InvokeRepeating(wc.CollectWater, interval, interval);
+                }
+                else wc.InvokeRandomized(wc.CollectWater, WaterCatcher.collectInterval, WaterCatcher.collectInterval, 6f);
+            }, 0.1f);
         }
 
         private void UpdateWaterPump(WaterPump pump, bool state)
         {
-            pump.PumpInterval = state ? _config.WaterPumpInterval : 20f;
-            pump.AmountPerPump = state ? _config.WaterPumpAmount : 30;
-            //pump.Invoke(() => pump.InvokeRandomized(pump.CreateWater, pump.PumpInterval, pump.PumpInterval, pump.PumpInterval * 0.1f), 0.1f);
+            pump.PumpInterval = state ? _config.WaterPump.Interval : 10f;
+            pump.AmountPerPump = state ? _config.WaterPump.Amount : 85;
+            pump.CancelInvoke(pump.CreateWater);
+            pump.InvokeRandomized(pump.CreateWater, pump.PumpInterval, pump.PumpInterval, pump.PumpInterval * 0.1f);
         }
 
         #region Configuration
 
         private Configuration _config;
 
+        private class SmallWaterCatcherSettings
+        {
+            [JsonProperty(PropertyName = "Stack Size (vanilla: 10000)")]
+            public int StackSize { get; set; } = 50000;
+
+            [JsonProperty(PropertyName = "Add Amount Of Water (vanilla: 10)")]
+            public float Amount { get; set; } = 20f;
+
+            [JsonProperty(PropertyName = "Add Water Every X Seconds (vanilla: 60)")]
+            public float Interval { get; set; } = 30f;
+        }
+
+        private class LargeWaterCatcherSettings
+        {
+            [JsonProperty(PropertyName = "Stack Size (vanilla: 50000)")]
+            public int StackSize { get; set; } = 250000;
+
+            [JsonProperty(PropertyName = "Add Amount Of Water (vanilla: 30)")]
+            public float Amount { get; set; } = 60f;
+
+            [JsonProperty(PropertyName = "Add Water Every X Seconds (vanilla: 60)")]
+            public float Interval { get; set; } = 30f;
+        }
+
+        private class WaterPumpSettings
+        {
+            //[JsonProperty(PropertyName = "Max Output (vanilla: 12)")]
+            //public int Output { get; set; } = 20;
+
+            [JsonProperty(PropertyName = "ML Capacity (vanilla: 2000)")]
+            public int StackSize { get; set; } = 10000;
+
+            [JsonProperty(PropertyName = "Add Amount Of Water To Water Pumps")]
+            public int Amount { get; set; } = 130;
+
+            [JsonProperty(PropertyName = "Add Water To Water Pumps Every X Seconds")]
+            public float Interval { get; set; } = 10f;
+        }
+
         private class Configuration
         {
+            [JsonProperty(PropertyName = "Small Water Catcher")]
+            public SmallWaterCatcherSettings SmallWaterCatcher { get; set; } = new SmallWaterCatcherSettings();
+
+            [JsonProperty(PropertyName = "Large Water Catcher")]
+            public LargeWaterCatcherSettings LargeWaterCatcher { get; set; } = new LargeWaterCatcherSettings();
+
+            [JsonProperty(PropertyName = "Water Pump")]
+            public WaterPumpSettings WaterPump { get; set; } = new WaterPumpSettings();
+
             [JsonProperty(PropertyName = "Water Jug ML Capacity (vanilla: 5000)")]
             public int WaterJug { get; set; } = 25000;
 
@@ -181,32 +235,11 @@ namespace Oxide.Plugins
             [JsonProperty(PropertyName = "Water Bucket ML Capacity (vanilla: 2000)")]
             public int WaterBucket { get; set; } = 10000;
 
-            [JsonProperty(PropertyName = "Water Pump ML Capacity (vanilla: 2000)")]
-            public int WaterPump { get; set; } = 10000;
-
-            [JsonProperty(PropertyName = "Add Amount Of Water To Water Pumps")]
-            public int WaterPumpAmount { get; set; } = 30;
-
-            [JsonProperty(PropertyName = "Add Water To Water Pumps Every X Seconds")]
-            public float WaterPumpInterval { get; set; } = 20f;
-
             [JsonProperty(PropertyName = "Water Purifier ML Capacity (vanilla: 5000)")]
             public int WaterPurifier { get; set; } = 25000;
 
             [JsonProperty(PropertyName = "Water Purifier (Powered) ML Capacity (vanilla: 5000)")]
             public int PoweredWaterPurifier { get; set; } = 25000;
-
-            [JsonProperty(PropertyName = "Large WaterCatcher Stack Size (vanilla: 50000)")]
-            public int StackSizeLarge { get; set; } = 250000;
-
-            [JsonProperty(PropertyName = "Small WaterCatcher Stack Size (vanilla: 10000)")]
-            public int StackSizeSmall { get; set; } = 50000;
-
-            [JsonProperty(PropertyName = "Add Amount Of Water To Water Catchers")]
-            public float WaterCatcherAmount { get; set; } = 10f;
-
-            [JsonProperty(PropertyName = "Add Water To Water Catchers Every X Seconds")]
-            public float WaterCatcherInterval { get; set; } = 20f;
         }
 
         protected override void LoadConfig()
